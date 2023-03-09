cb153ce54d848ebcb7dfec909dc53292b646e2a8
Charlie Morse <Charles.morse@mail.mcgill.ca>
2023-03-09 14:21:28 -0500

    Q2 20/25

--- A2/A2_Q2.java
+++ A2/A2_Q2.java
@@ -1,14 +1,14 @@
-import java.util.*;
+//import java.util.*;
 
 public class A2_Q2 {
 
 	public static int change(int[] denominations) {
 		int finalIndex = denominations.length - 1;
 		int bound = denominations[finalIndex] + denominations[finalIndex -1];
+		int[] dynamic = dynamic(denominations, bound, finalIndex);
 		for (int i = 2; i < bound; i++) {
-			int dynamic = dynamic(denominations, i);
 			int greedy = greedy(denominations, i, finalIndex);
-			if (dynamic != greedy) return greedy; //counterexample found => greedy fails
+			if (dynamic[i] != greedy) return i; //counterexample found => greedy fails
 		}
 		return -1; //if loops ends => no counterexample exists
 		
@@ -23,6 +23,7 @@ public class A2_Q2 {
 				total++;
 			}
 			if (value == 0) return total; //exit case
+			finalIndex --;
 		}
 
 		
@@ -30,10 +31,25 @@ public class A2_Q2 {
 	}
 
 
-	public static int dynamic(int[] denominations, int value) {
-		
+	public static int[] dynamic(int[] denominations, int value, int finalIndex) {
+		int refTable[] = new int[value + 1];
 
-		return -1;
+		//format table
+		refTable[0] = 0;
+		for (int i = 1; i <= value; i++) {
+			refTable[i] = Integer.MAX_VALUE;
+		}
+
+		for (int i = 1; i <= value; i++) {
+
+			for (int j = finalIndex; j >= 0; j--) {
+				if (denominations[j] <= i) { //if coin_j fits in i then
+					int workingTotal = refTable[i - denominations[j]]; //set total 0 if perfect 
+					if (workingTotal != Integer.MAX_VALUE && workingTotal + 1 < refTable[i]) refTable[i] = workingTotal + 1;
+				}
+				//otherwise move to next coin until one does fit
+			}
+		}
+		return refTable;
 	}
-
 }
