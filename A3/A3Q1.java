59dc371a2e89a3de5860883765149358cddcf2da
Charlie Morse <Charles.morse@mail.mcgill.ca>
2023-03-30 12:52:32 -0400

    create logic structure for Q1

--- A3/A3Q1.java
+++ A3/A3Q1.java
@@ -1,7 +1,61 @@
 import java.util.*;
 
+		//if a letter is on its boarder (L at bottom, U at top, etc) then consider it as a 1
+		//otherwise it can be accessed
+		//destination is at a building on the edge of the city
+
+		//say for some 
+		//	1 0 1 1
+		//  1 0 0 1
+		//  1 1 S 1
+		//  1 0 1 1
+		//has solution Up Left Up
+
+		//say for some
+		//  1 D 1 1
+		//  1 0 0 1
+		//  1 1 S 1
+		//  1 0 1 1
+		//has the same ssolution as above
+
 public class A3Q1 {
+
+	// public class Graph {
+
+	// 	public class Node {
+	// 		//fields
+	// 		public boolean canMove;
+	// 		public ArrayList<Node> nodes;
+
+	// 		public Node(boolean canMove) {
+	// 			this.canMove = canMove;
+	// 			this.nodes = new ArrayList<Node>();
+	// 		}
+
+	// 	}
+
+	// 	public Node startNode;
+	
+	
+	
+	
+	
+	// }
+
+
+
 	public static int find_exit(int time, String[][] board) {
-		
+		return 0;
+	}
+
+	public static void constructGraph(String[][] board) {
+		for (String[] column: board) {
+			for (String row: column) {
+
+			}
+		}
+	}
+
+	public static void findPath() {
 	}
 }
