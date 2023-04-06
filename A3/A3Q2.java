9dda24e3fb7fe045198aecee29a4a2e7b536bb46
Charlie Morse <Charles.morse@mail.mcgill.ca>
2023-04-05 23:54:21 -0400

    Q2 passing all

--- A3/A3Q2.java
+++ A3/A3Q2.java
@@ -2,72 +2,62 @@ import java.util.*;
 
 public class A3Q2 {
 
-	//begin by establishing graph data structure
-	public static class Graph {
-		//has both verteces and edges
-		public static class Vertex {
-			private int value;
-			private ArrayList<Edge> adjs;
-
-			public Vertex(int value) {
-				this.adjs = new ArrayList<>();
-				this.value = value;
-			}
-		
-			public int getValue() {
-				return this.value;
-			}
-
-			public ArrayList<Edge> getAdjacent() {
-				return this.adjs;
-			}
-
-			public void setValue(int value) {
-				this.value = value;
-			}
-
-			public void addEdge(Edge edge) {
-				adjs.add(edge);
-			}
-		}
-
-
-		public static class Edge {
-			private int weight;
-			private Vertex next;
-
-			public Edge(int weight, Vertex next) {
-				this.weight = weight;
-				this.next = next;
-			}
-
-			public int getWeight() {
-				return this.weight;
-			}
-
-			public Vertex getNext() {
-				return this.next;
-			}
-
-			public void setWeight(int weight) {
-				this.weight = weight;
-			}
-
-			public void setNext(Vertex vertex) {
-				this.next = vertex;
-			}
-		}
-
-	}
-
-
 	//pieces -> 
 	public static long[] num_pieces(long[] pieces, int[][] instructions) {
 
 		int modules = pieces.length;
-		
+		ArrayList<Integer> sort = new ArrayList<>();
 
+		//construct graph
+		ArrayList<Integer>[] graph = new ArrayList[modules];
+		for (int i = 0; i < modules; i++) {
+			//create arraylist for each spot in the array
+			graph[i] = new ArrayList<Integer>();
+		}
+		//add edges in graph
+		for (int[] instruction: instructions) {
+			graph[instruction[0]].add(instruction[1]);
+		}
+		//at this point the pieces dependencies have been 
+		//respected and the graph is created
+		//Topo sort
+		Stack<Integer> s = new Stack<>();
+		boolean[] map = new boolean[modules];
+		for (int i = 0; i < modules; i++) {
+			if (map[i]) continue;
+			//sort from provided source
+			dFS(graph, i, s, map);
+		}
+		//collapse stack
+		while(!s.isEmpty()) {
+			Integer item = s.pop();
+			sort.add(item);
+		}
+
+		//go through sorted list
+		for (int i = modules - 1; i >= 0; i--) {
+			//for each instruction sum the number of times a piece is needed
+			for (int j = 0; j < instructions.length; j++) {
+				//check if current instruction requires piece i
+				if (sort.get(i) != instructions[j][0]) continue;
+				pieces[sort.get(i)] += instructions[j][2]*pieces[instructions[j][1]];
+			}
+		}
 		return pieces;
+	}
+
+	public static void dFS(ArrayList<Integer>[] graph, int vertex, Stack<Integer> s, boolean[] map ) {
+		//visit vertex
+		//check adj
+		ArrayList<Integer> adjacents = graph[vertex];
+		map[vertex] = true;
+		for (int adjacent: adjacents) {
+			if (map[adjacent]) continue;
+			//search deeper
+			dFS(graph, adjacent, s, map);
+		}
+		//once this is reached then at end of the branch
+		s.push(vertex);
 	}
 
 }
